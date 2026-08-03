9ceb1c3286d5cf739439c91272ff440f892b88d8
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2026-08-03 20:05:16 -0300

    Add files via upload

---
+++ aula02/area_quadrado.php
@@ -0,0 +1,6 @@
+<?php
+    $valor = intval($_GET['valor']);
+    $area = $valor **2;
+
+    echo "A área do quadrado é de " . $area;
+?>
---
+++ aula02/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Principal - Área do quadrado</title>
+</head>
+<body>
+    <form action="area_quadrado.php">
+        <label for="">Lado</label>
+        <input type="text" name="valor">
+        <input type="submit" value="Calcular">
+    </form>
+</body>
+</html>
