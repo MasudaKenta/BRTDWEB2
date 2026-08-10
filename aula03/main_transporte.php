b5890a9fd39e9b15c24fa4bfa1081bf5d22151b7
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2026-08-10 20:08:49 -0300

    Add files via upload

---
+++ aula03/main_transporte.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transporte</title>
+</head>
+<body>
+    <form action="transporte.php">
+        <label for="">Informe o meio de transporte</label>
+        
+        <input type="text" name="transporte">
+        <br>
+        <input type="submit" value="Mostrar">
+    </form>
+</body>
+</html>
---
+++ aula03/transporte.php
@@ -0,0 +1,25 @@
+<?php
+
+    $transporte = $_GET['transporte'];
+    $transporte = trim($transporte);
+    $transporte = strtolower($transporte);
+
+    if($transporte == "carro"){
+        echo "<img src='imagens/carro.jpeg' alt='' width='400px' height='200px'>";
+    }
+    else if($transporte == "moto"){
+        echo "<img src='imagens/moto.jpg' alt='' width='400' height='200'>";
+    }
+    else if($transporte == "ônibus"){
+        echo "<img src='imagens/onibus' alt='' width='400' height='200'>";
+    }
+    else if($transporte == "avião"){
+        echo "<img src='imagens/aviao.jpg' alt='' width='400' height='200'>";
+    }
+    else if($transporte == "navio"){
+        echo "<img src='imagens/navio.jpg' alt='' width='400' height='200'>";
+    }
+    else
+        echo "Transporte não definido.";
+
+?>
