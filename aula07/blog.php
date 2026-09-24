76a3dbaaf2e4122de86c43514246984c4626ce40
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2026-09-24 20:26:29 -0300

    Update blog.php

--- aula07/blog.php
+++ aula07/blog.php
@@ -32,11 +32,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+ 
 </head>
 <body>
+    <?php
+        echo "Bem-vindo, $nome!";
+    ?>
     <form action="inserir.php">
         <!-- <label for=""></label> -->
+        <br>
         <textarea name="texto"></textarea>
+        <br><br>
+        <label for="">Insira uma imagem</label>
+        <br>
+        <input type="file" name="arquivo">
+        <input type="hidden" name="secret" value="<?php echo $nome?>">
+
     </form>
 </body>
 </html>
